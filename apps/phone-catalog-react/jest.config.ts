9ab02398e34662f95e73611f7591fbc95a1b4fe6
Alvaro <aldc30sc@gmail.com>
2022-08-30 14:32:36 +0200

    chore: :wrench: configure test for react app

--- apps/phone-catalog-react/jest.config.ts
+++ apps/phone-catalog-react/jest.config.ts
@@ -3,9 +3,9 @@ export default {
   displayName: 'phone-catalog-react',
   preset: '../../jest.preset.js',
   transform: {
-    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
-    '^.+\\.[tj]sx?$': 'babel-jest',
+    '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/apps/phone-catalog-react',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
 };
