92e938c3ec27e695879ed1c764c43e86a1d38b3f
Alvaro <aldc30sc@gmail.com>
2022-09-01 00:11:10 +0200

    feat: :wrench: configure express to access server images

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
 
 import { addPhoneRoutes } from './app/phones';
 
@@ -7,6 +8,9 @@ const app = express();
 
 //make use of cors
 app.use(cors());
+
+// access server images (there is room for improvement here)
+app.use(express.static(path.join(__dirname, '../../../apps/api/data/img')));
 
 // parse application/json
 app.use(express.json());
@@ -17,7 +21,7 @@ app.get('/api', (req, res) => {
 
 addPhoneRoutes(app);
 
-const port = process.env.port || 3333;
+const port = 3333;
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
