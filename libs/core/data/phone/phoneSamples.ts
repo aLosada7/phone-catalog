7aa1155c377a046a9d4c175a86e5e0d95d128054
Alvaro <aldc30sc@gmail.com>
2022-09-01 00:10:50 +0200

    chore: :memo: update samples with images

--- libs/core/data/phone/phoneSamples.ts
+++ libs/core/data/phone/phoneSamples.ts
@@ -4,32 +4,32 @@ export const PHONE_SAMPLE_1 = {
   description: 'lorem ipsum dolor sit amet consectetur.',
   color: 'black',
   price: 769,
-  imageFileName: 'IPhone_7.png',
+  imageFileName: 'iPhone7.jpeg',
   screen: '4,7 inch IPS',
   processor: 'A10 Fusion',
   ram: 2,
 };
 
 export const PHONE_SAMPLE_2 = {
-  name: 'iPhone 7',
-  manufacturer: 'Apple',
+  name: 'Samsung Galaxy 8',
+  manufacturer: 'Samsung',
   description: 'lorem ipsum dolor sit amet consectetur.',
-  color: 'black',
-  price: 769,
-  imageFileName: 'IPhone_7.png',
-  screen: '4,7 inch IPS',
+  color: 'white',
+  price: 599,
+  imageFileName: 'samsungGalaxy8.jpeg',
+  screen: '6,2 inch IPS',
   processor: 'A10 Fusion',
   ram: 2,
 };
 
 export const PHONE_SAMPLE_3 = {
-  name: 'iPhone 7',
+  name: 'iPhone 12',
   manufacturer: 'Apple',
   description: 'lorem ipsum dolor sit amet consectetur.',
   color: 'black',
   price: 769,
-  imageFileName: 'IPhone_7.png',
-  screen: '4,7 inch IPS',
+  imageFileName: 'iPhone12.png',
+  screen: '5,5 inch IPS',
   processor: 'A10 Fusion',
   ram: 2,
 };
